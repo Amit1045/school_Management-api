56b92b175e0b83325a82424784b23229ad47c140
genius24 <amitya9854@gmail.com>
2025-08-27 11:23:23 +0530

    Database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
+// import url from "url"
 
 dotenv.config({ path: "./.env" });
 console.log(process.env.MYSQLDATABASE);
@@ -8,12 +9,14 @@ let connection;
 
 export const connectDB = () => {
   try {
+    const dbUrl = process.env.DATABASE_URL;
+    const parsedUrl = new URL(dbUrl);
     connection = mysql.createConnection({
-        host: process.env.MYSQLHOST,
-        user: process.env.MYSQLUSER,
-        password: process.env.MYSQLPASSWORD ,
-        database: process.env.MYSQLDATABASE,
-        port: process.env.MYSQLPORT 
+        host: process.env.MYSQLHOST || parsedUrl.hostname ,
+        user: process.env.MYSQLUSER || parsedUrl.username,
+        password: process.env.MYSQLPASSWORD || password ,
+        database: process.env.MYSQLDATABASE || parsedUrl.pathname.replace("/", ""),
+        port: process.env.MYSQLPORT || parsedUrl.port
     });
 
     connection.connect((err) => {
